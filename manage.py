c648d0c50675ba48389961855412e7e4119ed5b8
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-07-13 18:18:58 +0300

    0. Set Up Django Project with Dockerized PostgreSQL and Redis

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trizaone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx-backend-caching_property_listings.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
