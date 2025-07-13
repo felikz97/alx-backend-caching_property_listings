3f7baad440f97a83ff6d139bbcee6b1ae4a16d9c
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-07-13 18:22:30 +0300

    project updates

--- alx-backend-caching_property_listings/settings.py
+++ alx-backend-caching_property_listings/settings.py
@@ -81,13 +81,28 @@ WSGI_APPLICATION = 'alx-backend-caching_property_listings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 
 # Password validation
